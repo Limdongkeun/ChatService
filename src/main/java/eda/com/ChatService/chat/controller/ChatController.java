a940eb447805258be2ad9cf70b8d03da8fa641a6
Dong Keun Lim <lim961007@naver.com>
2023-10-24 11:25:44 +0900

    @MessageMapping을 통해 websocket으로 들어오는 메시지 처리

--- src/main/java/eda/com/ChatService/chat/controller/ChatController.java
+++ src/main/java/eda/com/ChatService/chat/controller/ChatController.java
@@ -1,28 +1,25 @@
 package eda.com.ChatService.chat.controller;
 
-import eda.com.ChatService.chat.ChatRoom;
-import eda.com.ChatService.chat.service.ChatService;
-import java.util.List;
+import eda.com.ChatService.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
   
-  private final ChatService chatService;
+  private final SimpMessageSendingOperations messagingTemplate;
   
-  @PostMapping
-  public ChatRoom createRoom(@RequestParam String name) {
-    return chatService.createRoom(name);
-  }
-  
-  @GetMapping
-  public List<ChatRoom> finalAllRoom() {
-    return chatService.findAllRoom();
+  @MessageMapping("/message")
+  public void message(ChatMessage message) {
+    
+    if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+      message.setMessage(message.getSender() + "님이 입장하셨습니다");
+    }
+    messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
   }
 }
