4048ba4e80763fa60ae9963bc63a19a4c38305ac
Dong Keun Lim <lim961007@naver.com>
2023-10-25 17:25:34 +0900

    redisConfig 추가

---
+++ src/main/java/eda/com/ChatService/commen/redis/config/EmbeddedRedisConfig.java
@@ -0,0 +1,38 @@
+package eda.com.ChatService.commen.redis.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+  
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+  
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+  
+  private RedisServer redisServer;
+  
+  @PostConstruct
+  public void redisServer() {
+    redisServer = RedisServer.builder()
+      .port(redisPort)
+      .setting("maxmemory 128M")
+      .build();
+    redisServer.start();
+  }
+  
+  @PreDestroy
+  public void stopRedis() {
+    
+    if(redisServer != null) {
+      redisServer.stop();
+    }
+  }
+}
