8c559d55d5e19d59ac1f6cbb323a5a639323db4d
Dong Keun Lim <lim961007@naver.com>
2023-10-30 15:00:32 +0900

    local redis 추가하여 실행 두개의 환경에서 서로 대화 확인

--- src/main/java/eda/com/ChatService/chat/dto/ChatRoom.java
+++ src/main/java/eda/com/ChatService/chat/dto/ChatRoom.java
@@ -1,12 +1,17 @@
 package eda.com.ChatService.chat.dto;
 
+import java.io.Serializable;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+  
+  //클래스의 직렬화 버전을 관리하는 데 사용되며, 클래스가 변경될 때 해당 값을 업데이트하여 이전 버전과의 호환성을 유지하는 중요한 역할
+  private static final long serialVersionUID = 6494678977089006639L;
+  
   private String roomId;
   private String name;
   
